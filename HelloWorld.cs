bda2953c5f6832e9be24429853cdb0d7db2f7b44
Vovan1202 <kdkfdffd@gmail.com>
2024-11-09 19:06:05 +0300

    Add files via upload
    Wow script hello world

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    //start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject);
+    }
+
+    //update is called one per frame
+    void Update()
+    {
+        print("cricket");
+    }
+}
